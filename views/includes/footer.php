f02e669f57718dec56e6263ef5c25032c38d4c6c
Đức Đào <duc.dao.431@gmail.com>
2023-06-27 17:36:21 +0700

    🐞 fix: fix FunFact component

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,19 +1,25 @@
 <footer class="pt-3 pb-5 mt-4 bg-info text-light">
-    <div class="container pb-4 pt-3 d-flex justify-content-between align-items-center">
+    <div
+        class="container pb-4 pt-3 d-flex justify-content-between align-items-center">
         <h3><i class="fas fa-icons"></i> Bludist</h3>
-        <p class="col-3 overflow-auto" style="min-height: 70px; max-height: 70px;">
+        <p class="col-3 overflow-auto"
+            style="min-height: 70px; max-height: 70px;">
             <b>Fun fact:</b>
             <span id="fun-fact"></span>
-            <script src="./controllers/FunFact.js" type="module"></script>
+            <script src="<?= ROOT ?>controllers/FunFact.js" type="module">
+            </script>
         </p>
     </div>
 </footer>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js">
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js">
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js">
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js">
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js">
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js">
 </script>
 
 </body>
